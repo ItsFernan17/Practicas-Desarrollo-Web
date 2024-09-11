6819065f21d89108bf6173bccff9c22b08317299
Fernando Rivas <frivasm@miumg.edu.gt>
2024-09-11 15:03:11 -0600

    Arreglo Validaciones

--- src/productos/dto/create-producto.dto.ts
+++ src/productos/dto/create-producto.dto.ts
@@ -1,26 +1,26 @@
-import { IsNotEmpty, IsNumber, IsString, MaxLength, MinLength, Min, IsOptional } from "class-validator";
+import { IsNotEmpty, IsNumber, IsInt, IsString, MaxLength, MinLength, Min, IsOptional } from "class-validator";
 
 
 export class CreateProductoDto{
 
     @IsString()
-    @IsNotEmpty()
-    @MinLength(3)
-    @MaxLength(50)
+    @IsNotEmpty({message: "Este campo es obligatorio"})
+    @MinLength(3, {message: "El nombre debe de tener mínimo 3 caracteres"})
+    @MaxLength(50, {message: "El nombre debe de tener máximo 50 caracteres"})
     readonly nombre: string;
 
     @IsString()
-    @MaxLength(90)
+    @MaxLength(90, {message: "La descripción del producto debe de tener máximo 90 caracteres"})
     @IsOptional()
     readonly descripcion?: string;
 
     @IsNumber()
-    @IsNotEmpty()
-    @Min(0.5)
+    @IsNotEmpty({message: "Este campo es obligatorio"})
+    @Min(0.5, {message: "El precio debe de ser mayor que 0"})
     readonly  precio: number;
 
-    @IsNumber()
-    @IsNotEmpty()
-    @Min(0)
+    @IsInt({message: "El stock debe ser un número entero"})
+    @IsNotEmpty({message: "Este campo es obligatorio"})
+    @Min(0, {message: "El stock debe ser mayor o igual a 0"})
     readonly stock: number;
 }
--- src/productos/dto/update-producto.dto.ts
+++ src/productos/dto/update-producto.dto.ts
@@ -1,25 +1,25 @@
-import { IsNumber, IsString, MaxLength, MinLength, Min, IsOptional } from "class-validator";
+import { IsNumber, IsString, MaxLength, MinLength, Min, IsOptional, IsInt } from "class-validator";
 
 export class UpdateProductoDto{
 
     @IsString()
     @IsOptional()
-    @MinLength(3)
-    @MaxLength(50)
+    @MinLength(3, {message: "El nombre debe de tener mínimo 3 caracteres"})
+    @MaxLength(50, {message: "El nombre debe de tener máximo 50 caracteres"})
     readonly nombre?: string;
 
     @IsString()
-    @MaxLength(90)
+    @MaxLength(90, {message: "La descripción del producto debe de tener máximo 90 caracteres"})
     @IsOptional()
     readonly descripcion?: string;
 
     @IsNumber()
     @IsOptional()
-    @Min(0.5)
+    @Min(0.5, {message: "El precio debe de ser mayor que 0"})
     readonly precio?: number;
 
-    @IsNumber()
+    @IsInt({message: "El stock debe ser un número entero"})
     @IsOptional()
-    @Min(0)
+    @Min(0, {message: "El stock debe ser mayor o igual a 0"})
     readonly stock?: number;
 }
